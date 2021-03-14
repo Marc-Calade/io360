246bd18a397e0224b4836e7335e4df59e0eb2887
Marc-Calade <marc.calade@gmail.com>
2021-03-14 14:06:05 +0100

    version-002 io360 app moved into github

--- index.js
+++ index.js
@@ -3,51 +3,45 @@ const express = require('express');
 const listeCourses = require('./listeCourses.json');
 fs = require('fs');
 const app = express();
-
+// Configuration des options cors
 const cors = require('cors-express');
 options = {
-    allow: {
-        origin: '*',
-        methods: 'GET,PATCH,PUT,POST,DELETE,HEAD,OPTIONS',
-        headers: 'Content-Type, Authorization, Content-Length, X-Requested-With, X-HTTP-Method-Override'
-    }
+  allow: {
+    origin: '*',
+    methods: 'GET,PATCH,PUT,POST,DELETE,HEAD,OPTIONS',
+    headers: 'Content-Type, Authorization, Content-Length, X-Requested-With, X-HTTP-Method-Override'
+  }
 };
-
 // Middleware pour faire un post
 app.use(express.json());
 app.use(cors('*'));
-
-// Renvoi le fichier listeCourses
+// Envoi du fichier listeCourses vers le client
 app.get('/listeCourses', (req, res, next) => {
- console.log('app.get')
-    res.send(listeCourses);
+  console.log('app.get')
+  res.send(listeCourses);
 });
-
 // Enregistre les données dans le fichier
-app.post('/listeCourses/:id', (req, res, next) => {  
-    // id from url
-    const produitId = req.params.id;
-    //update data
-    const listeData = req.body;
-    //res.status(200).json(produitData);
-    res.status(200);
-    console.log('app.post: params.id = ' + produitId);
-    // Enregistrement dans le fichier listeCourses.json
-    //fs.writeFile('./parkings.json', JSON.stringify(parkings, null, 4), function (err) {
-    fs.writeFile('./listeCourses.json', JSON.stringify(listeData, null, 4), function (err) {  
+app.post('/listeCourses/:id', (req, res, next) => {
+  // id from url
+  const produitId = req.params.id;
+  //update data
+  const listeData = req.body;
+  // Mise à jour du status code de la réponse
+  res.status(200);
+  // Enregistrement dans le fichier listeCourses.json
+  fs.writeFile('./listeCourses.json', JSON.stringify(listeData, null, 4), function (err) {
     if (err) throw err;
-      console.log('Saved!');
-  }); 
-    if (res.ok) {
-      res.status(201).send();
-      console.log('reponse ok: ');
-    } else {
-      res.status(400);
-    }
+    res.status(201).send();
   });
-
-  // Serveur l’écoute avec la méthode listen avec app + le port 
+  if (res.ok) {
+    res.status(202).send();
+    console.log('app.post')
+  } else {
+    res.status(400);
+  }
+});
+// Serveur l’écoute avec la méthode listen avec app + le port 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log('Serveur à l\'écoute');
+  console.log('Serveur à l\'écoute');
 });
